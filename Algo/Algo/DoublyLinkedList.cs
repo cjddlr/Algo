a047fba82b835d99a45ac210e7491be1fce70ed9
DESKTOP-RKJORPL\ChungIk <cjddlr1@hotmail.com>
2022-10-05 23:56:11 +0900

    Doubly Linked List 시작

---
+++ Algo/Algo/DoublyLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+    public class DoublyLinkedList
+    {
+        class Node
+        {
+            public Node(int data)
+            {
+                this.data = data; 
+            }
+
+            int data;
+            Node prev;
+            Node next;
+        }
+
+        void AddFirst(int data)
+        {
+
+        }
+
+        void AddLast(int data)
+        {
+
+        }
+
+        void AddAt(int index, int data)
+        {
+
+        }
+
+        void RemoveFirst()
+        {
+
+        }
+
+        void RemoveLast()
+        {
+
+        }
+
+        void RemoveAt(int index)
+        {
+
+        }
+    }
+}
